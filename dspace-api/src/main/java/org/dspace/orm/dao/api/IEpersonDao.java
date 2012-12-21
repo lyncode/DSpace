b5c98eedebbf15eab21241e370826b27be059003
Miguel Pinto <mpinto@lyncode.com>
2012-12-21 00:07:33 +0000

    EPerson + EPersonGroup

--- dspace-api/src/main/java/org/dspace/orm/dao/api/IEpersonDao.java
+++ dspace-api/src/main/java/org/dspace/orm/dao/api/IEpersonDao.java
@@ -1,8 +1,14 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.orm.dao.api;
 
-import org.dspace.orm.entity.EPerson;
+import org.dspace.orm.entity.Eperson;
 
-public interface IEPersonDao {
-	EPerson findById(int id);
-	EPerson findByEmail(String email);
-}
+public interface IEpersonDao extends IDSpaceDao<Eperson>{
+    
+   }
