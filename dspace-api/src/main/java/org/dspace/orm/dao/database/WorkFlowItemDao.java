7a492366ce3b7e25da2dd16bc88832344fc0a31c
Miguel Pinto <mpinto@lyncode.com>
2012-12-21 22:38:57 +0000

    Add WorkFlowItem entity.

---
+++ dspace-api/src/main/java/org/dspace/orm/dao/database/WorkFlowItemDao.java
@@ -0,0 +1,23 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.orm.dao.database;
+
+import org.dspace.orm.dao.api.IWorkFlowItemDao;
+
+import org.dspace.orm.entity.WorkFlowItem;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Repository("IWorkFlowItemDao")
+public class WorkFlowItemDao extends DSpaceDao<WorkFlowItem> implements IWorkFlowItemDao {
+    
+	public WorkFlowItemDao() {
+		super(WorkFlowItem.class);
+	}
+}
--- dspace-api/src/main/java/org/dspace/orm/entity/WorkFlowItem.java
+++ dspace-api/src/main/java/org/dspace/orm/entity/WorkFlowItem.java
@@ -19,17 +19,16 @@ import javax.persistence.Table;
 import org.dspace.core.Constants;
 
 @Entity
-@Table(name = "workspaceitem")
-public class WorkSpaceItem implements IDSpaceObject{
+@Table(name = "workflowitem")
+public class WorkFlowItem implements IDSpaceObject{
     private int id;
-    
     private Item item;
     private Collection collection;
+    private Integer state;
+    private Eperson owner;
     private boolean multipleTitles;
     private boolean publishedBefore;
     private boolean multipleFiles;
-    private Integer stageReached;
-    private Integer pageReached;
     
     @Id
     @Column(name = "workspace_item_id")
@@ -71,6 +70,43 @@ public class WorkSpaceItem implements IDSpaceObject{
 		this.collection = collection;
 	}
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner", nullable = true)
+	public Eperson getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Eperson owner) {
+		this.owner = owner;
+	}
+
+	@Column(name = "state", nullable = true)
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	@Column(name = "published_before", nullable = true)
+	public boolean isPublishedBefore() {
+		return publishedBefore;
+	}
+
+	public void setPublishedBefore(boolean publishedBefore) {
+		this.publishedBefore = publishedBefore;
+	}
+
+	@Column(name = "multiple_titles", nullable = true)
+	public boolean isMultipleTitles() {
+		return multipleTitles;
+	}
+
+	public void setMultipleTitles(boolean multipleTitles) {
+		this.multipleTitles = multipleTitles;
+	}
+
 	@Column(name = "multiple_files", nullable = true)
 	public boolean isMultipleFiles() {
 		return multipleFiles;
@@ -80,40 +116,7 @@ public class WorkSpaceItem implements IDSpaceObject{
 		this.multipleFiles = multipleFiles;
 	}
 
-    @Column(name = "published_before", nullable = true)
-	public boolean isPublishedBefore() {
-		return publishedBefore;
-	}
-
-	public void setPublishedBefore(boolean publishedBefore) {
-		this.publishedBefore = publishedBefore;
-	}
 	
-    @Column(name = "stage_reached", nullable = true)
-	public Integer getStageReached() {
-		return stageReached;
-	}
-
-	public void setStageReached(Integer stageReached) {
-		this.stageReached = stageReached;
-	}
-
-	@Column(name = "page_reached", nullable = true)
-	public Integer getPageReached() {
-		return pageReached;
-	}
-
-	public void setPageReached(Integer pageReached) {
-		this.pageReached = pageReached;
-	}
-	@Column(name = "multiple_titles", nullable = true)
-	public boolean isMultipleTitles() {
-		return multipleTitles;
-	}
-
-	public void setMultipleTitles(boolean multipleTitles) {
-		this.multipleTitles = multipleTitles;
-	}
 
    
 }
