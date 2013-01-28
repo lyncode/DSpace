f0055b6e0cc4ed3c6dc32d9729382d75fd5b0054
lyncodev <jmelo@lyncode.com>
2013-01-28 20:20:04 +0000

    Member controller depending on DSpace Services

--- dspace-springui/src/main/java/org/dspace/springui/controllers/MemberController.java
+++ dspace-springui/src/main/java/org/dspace/springui/controllers/MemberController.java
@@ -9,15 +9,19 @@ package org.dspace.springui.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * Member controller
+ * 
  * @author João Melo <jmelo@lyncode.com>
  *
  */
 @Controller
+@DependsOn("dspaceServices")
 public class MemberController {
 	@RequestMapping(value="/login")
 	public String loginAction (HttpServletRequest req, ModelMap model) {
